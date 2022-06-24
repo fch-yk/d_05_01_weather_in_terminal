467e7f7807127a8edee8117656895d9a3b66e776
yk <yefim_korshever@mail.ru>
2022-06-24 18:28:27 +0300

    Remove unordered feature

--- weather.py
+++ weather.py
@@ -1,11 +1,6 @@
 import requests
 
 
-def check_for_redirect(response):
-    if response.history:
-        raise requests.HTTPError('redirection detected')
-
-
 def main():
     locations = ('Лондон', 'svo', 'Череповец')
     payload = {'nTqm': '', 'lang': 'ru'}
@@ -14,7 +9,6 @@ def main():
         url = url_template.format(location)
         response = requests.get(url, params=payload)
         response.raise_for_status()
-        check_for_redirect(response)
         print(response.text)
 
 
